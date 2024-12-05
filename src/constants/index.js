88a646ec7902bd7c0f0a7937d063f95c1fbf0516
SidW111 <111293278+SidW111@users.noreply.github.com>
2024-12-05 21:52:31 +0530

    Your commit message

---
+++ src/constants/index.js
@@ -0,0 +1,59 @@
+
+
+
+export const navbar = [{
+    id:1,
+    title:"Home"
+},{
+    id:2,
+    title:"About"
+},{
+    id:3,
+    title:"Service"
+}]
+
+export const services = [{
+    id:1,
+    title:"Website development",
+    description:"We create visually stunning websites that reflect your brand's personality. Your website will be fully responsive, ensuring an optimal viewing experience ond desktops, tablets, and smartphones. "
+},{
+    id:2,
+    title:"Website Design",
+    description:"We prioritize UI/UX design principles to create unique & aesthetically pleasing designs. The designs will be fully responsive, trending and high quality."
+},{
+    id:3,
+    title:"Website Redesign",
+    description:"We transform your website into a powerful marketing tool with custom redesign strategy. The website will generate greater leads and impact on your brand."
+}]
+
+export const work = [{
+    id:1,
+    Image:"/src/assets/Service-bg.png",
+    url:"/"
+},{
+    id:2,
+    Image:"/src/assets/Service-bg.png",
+    url:"/"
+},{
+    id:3,
+    Image:"src/assets/Service-bg.png",
+    url:"/home"
+},{
+    id:4,
+    Image:"src/assets/Service-bg.png",
+    url:"/"
+}]
+
+export const footer = [{
+    id:1,
+    title:"About us",
+    description:["Team","Our story"]
+},{
+    id:2,
+    title:"Services",
+    description:["Web design","Web dev","Web redesign"]
+},{
+    id:3,
+    title:"Contact",
+    description:["+91 000000000","xyz@gmail.com"]
+}]
